797e6b023553f2cfc55ae899d9e0436ef521207c
dileigege <1637393255@qq.com>
2020-03-12 15:41:28 +0800

    目录提交-项目准备完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 模块
+ * @Author: DA-HUA
+ * @Date: 2020-02-10 16:54:56
+ * @LastEditTime: 2020-03-12 14:58:20
+ * @LastEditors: 小刀
+ */
 module.exports = {
   root: true,
   env: {
@@ -11,8 +18,13 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    "space-before-function-paren": 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "always",
+      "asyncArrow": "always"
+    }], 
+     'semi':['err','always']
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
-<!--
- * @Description: 首页模块
- * @Author: 前端-张晓伦
- * @Date: 2020-02-10 16:54:56
- * @LastEditTime: 2020-03-12 12:59:09
- * @LastEditors: 小刀
- -->
 <template>
-  <div></div>
+  <div>
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data() {
+  data () {
     return {}
   }
 }
